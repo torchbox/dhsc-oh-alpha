994cf9853e7b6cac1e17199668fa320b5d353b4d
Kevin Howbrook <kevin.howbrook@torchbox.com>
2022-02-16 11:00:11 +0000

    Can be more specific with this exception

--- alpha/registration/views.py
+++ alpha/registration/views.py
@@ -37,7 +37,7 @@ class OrganisationSelectInput(TemplateView):
         try:
             # Have to search for the provider by name here
             provider = Provider.objects.get(name=provider)
-        except Exception:
+        except Provider.DoesNotExist:
             # No provider, reload the search with a general error
             context["error"] = True
             return self.render_to_response(context)
