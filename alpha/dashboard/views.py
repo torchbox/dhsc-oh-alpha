9df64093cef62f4f3e90a7720aa73fb67d39da63
Kev <kevin.howbrook@torchbox.com>
2022-03-03 14:35:10 +0000

    Populated and non populated org view

--- alpha/dashboard/views.py
+++ alpha/dashboard/views.py
@@ -6,6 +6,14 @@ class Home(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+
         context["organisation"] = "Occupational Health For All"
         context["user"] = {"name": "Sam", "email": "sam@ohforall.com"}
+
+        # Control showing a different include in the template if the url
+        # has dashboard/?org_data=True
+        org_data = self.request.GET.get("org_data", "")
+        if org_data:
+            context["organisation_data"] = True
+
         return context
